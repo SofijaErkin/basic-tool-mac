0071f1bb8cb89ef3b1692b3edd448b9473c7a9bc
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-12-08 08:32:13 +0800

    Update mongodb-problem-build.cmake
    Add an address of the problem in stackoverflow.

--- mongodb/mongodb-problem-build.cmake
+++ mongodb/mongodb-problem-build.cmake
@@ -1,5 +1,7 @@
 # # Problem: MongoDB: exception in initAndListen: 
 # # 20 Attempted to create a lock file on a read-only directory: /data/db, terminating?
+# # stackoverflow:
+# # https://stackoverflow.com/questions/42446931/mongodb-exception-in-initandlisten-20-attempted-to-create-a-lock-file-on-a-rea/70231134#70231134
 # [[
 wrong reason:
 
