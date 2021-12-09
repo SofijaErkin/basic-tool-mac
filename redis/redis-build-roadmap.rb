d53f930c6c91eb9734b744ef7347b9b1704c4e32
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-12-09 13:32:08 +0800

    Update redis-build-roadmap.rb
    Add a set of roadmaps for building Redis on mac.

--- redis/redis-build-roadmap.rb
+++ redis/redis-build-roadmap.rb
@@ -1,9 +1,38 @@
-# Problem:How to install MySQL 5.7.x on Mac OS Sierra
-# StackOverFlow:
-# https://stackoverflow.com/questions/41097733/how-to-install-mysql-5-7-x-on-mac-os-sierra
+# Problem:How to install Redis x.x.x on Mac OS 
+# RoadMap:
 =begin
-You know, after MySQL is uninstalled and reinstalled via homebrew, it will prompt: 1. Upgrade to macOS 10.13; 2. Upgrade to Xcode 9.2.
-If you don't want to upgrade the system or Xcode version, then request the MySQL official website to download:
-https://downloads.mysql.com/archives/community/
-The MySQL version suitable for macOS 10.12 ranges from 5.7.17 to 5.7.23.
+You know, 
+1.download redis;
+
+2.ln -s;
+
+3.sudo make;
+
+4.sudo install;
+
+5.sudo mkdir -p /usr/local/redis/bin;
+
+6.sudo cp -r /usr/local/redis/src/... /usr/local/redis/bin/...;
+
+7.sudo cp -r /usr/local/redis/redis/conf /usr/local/etc/redis/conf;
+
+8.sudo mkdir -p /usr/local/var/log/redis;
+
+9.$Path;
+
+10.vim ./redis.conf;
+
+11. ./redis-server ./etc/redis/conf;
+
+12. redis-server;
+
+13. redis-cli;
+
+14.shutdown;
+
+15. vim ./redis.conf for requipentpass changing;
+
+16.VS code or others Desktop.
+
+The Redis version suitable for macOS 1x.x ranges from 3.2.x to 4.0.x, or even the lastest version.
 =end
