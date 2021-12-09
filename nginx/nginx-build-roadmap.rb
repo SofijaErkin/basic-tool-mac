5b659a6816c1d65dc7b18f651346dc6b9b58617b
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-12-10 07:49:02 +0800

    Update nginx-build-roadmap.rb
    Add a set of roadmaps to build Nginx on mac.

--- nginx/nginx-build-roadmap.rb
+++ nginx/nginx-build-roadmap.rb
@@ -2,6 +2,22 @@
 # RoadMap:
 =begin
 You know, 
-
-The Nginx version suitable for macOS 1x.x ranges from x.x.x to x.x.x, or even the lastest version.
+1.download;
+2.tar;
+3.sudo ln -s;
+4../con ../pcre ../zlib ../openssl;
+5.sudo make;
+6.sudo make install;
+7.$Path;
+8.sudo mv ./nginx/nginx.conf ./etc/nginx.conf;
+9.sudo mv ./etc/nginx/servers ./local/nginx/bin/servers;
+10.vim ./etc/nginx.conf;
+11.sudo ./local/nginx ./etc/nginx.conf;
+12.nginx;
+13.localhost:8080;
+14.vim ./etc/nginx.conf;
+15.nginx -s reload;
+16.nginx -s stop;
+17.nginx -s quit;
+The Nginx version suitable for macOS 1x.x.x ranges from 1.11.13 to 1.21.4(the lastest version).
 =end
