69ff189e025f2ff328e0e663f5eed71a0e89a7c3
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-12-09 13:16:44 +0800

    Create redis-build-roadmap.rb
    Add a roadmap for building Redis on mac.

---
+++ redis/redis-build-roadmap.rb
@@ -0,0 +1,9 @@
+# Problem:How to install MySQL 5.7.x on Mac OS Sierra
+# StackOverFlow:
+# https://stackoverflow.com/questions/41097733/how-to-install-mysql-5-7-x-on-mac-os-sierra
+=begin
+You know, after MySQL is uninstalled and reinstalled via homebrew, it will prompt: 1. Upgrade to macOS 10.13; 2. Upgrade to Xcode 9.2.
+If you don't want to upgrade the system or Xcode version, then request the MySQL official website to download:
+https://downloads.mysql.com/archives/community/
+The MySQL version suitable for macOS 10.12 ranges from 5.7.17 to 5.7.23.
+=end
