3759e96d06829ff1a363fb1cab08913e587344ae
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-12-09 13:53:18 +0800

    Update mongodb-download-roadmap.c
    Add a set of roadmaps of building MongoDB on mac.

--- mongodb/mongodb-download-roadmap.c
+++ mongodb/mongodb-download-roadmap.c
@@ -1,4 +1,17 @@
 // This is a roadmap for downloading MongoDB on macOS.
 /*
+You know, 
+1.download mongodb;
+2.ln -s;
 
+8.sudo mkdir -p /usr/local/var/log/mongodb;
+9.$Path;
+10.vim ./mongod.conf;
+11. ./mongod ./etc/mongodb/conf;
+12.mongod;
+13.mongo;
+14.shutdownService();
+15. user-root user-pass
+16.VS code or others Desktop.
+The MongoDB version suitable for macOS 1x.x ranges from 4.0.x to 6.x.x, or even the lastest version.
 */
