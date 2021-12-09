615e24f9f19669a7ec4b4f459e538d89a0ba5d04
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-12-09 13:45:29 +0800

    Update nginx-build-roadmap.rb
    Minus a set of others.

--- nginx/nginx-build-roadmap.rb
+++ nginx/nginx-build-roadmap.rb
@@ -1,22 +1,7 @@
-# Problem:How to install Redis x.x.x on Mac OS 
+# Problem:How to build Nginx x.x.x on Mac OS 
 # RoadMap:
 =begin
 You know, 
-1.download redis;
-2.ln -s;
-3.sudo make;
-4.sudo install;
-5.sudo mkdir -p /usr/local/redis/bin;
-6.sudo cp -r /usr/local/redis/src/... /usr/local/redis/bin/...;
-7.sudo cp -r /usr/local/redis/redis/conf /usr/local/etc/redis/conf;
-8.sudo mkdir -p /usr/local/var/log/redis;
-9.$Path;
-10.vim ./redis.conf;
-11. ./redis-server ./etc/redis/conf;
-12. redis-server;
-13. redis-cli;
-14.shutdown;
-15. vim ./redis.conf for requipentpass changing;
-16.VS code or others Desktop.
-The Redis version suitable for macOS 1x.x ranges from 3.2.x to 4.0.x, or even the lastest version.
+
+The Nginx version suitable for macOS 1x.x ranges from x.x.x to x.x.x, or even the lastest version.
 =end
