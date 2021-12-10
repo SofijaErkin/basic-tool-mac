4b2771acffd5dc7a3f55bd25d649cca31457b0fb
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-12-10 11:34:27 +0800

    Create leveldb-build-roadmap.cmake
    Add a set of references for building levelDB on mac.

---
+++ leveldb/leveldb-build-roadmap.cmake
@@ -0,0 +1,26 @@
+# # "# # " for one line comment
+# #
+# # "# [[ ... ]]" for more than one lines comment
+# [[
+
+
+
+Other reference:
+1.homebrew-core/Formula/leveldb.rb:
+("Key-value storage library with ordered mapping")
+homepage "https://github.com/google/leveldb/"
+sha256 "9a37f8a6174f09bd622bc723b55881dc541cd50747cbd08831c2a82d620f6d76"
+license "BSD-3-Clause"
+https://github.com/Homebrew/homebrew-core/blob/master/Formula/leveldb.rb
+
+2.homebrew-core/Formula/redis-leveldb.rb:
+("Redis-protocol compatible frontend to leveldb")
+homepage "https://github.com/KDr2/redis-leveldb"
+sha256 "b34365ca5b788c47b116ea8f86a7a409b765440361b6c21a46161a66f631797c"
+https://github.com/Homebrew/homebrew-core/blob/master/Formula/redis-leveldb.rb
+
+
+
+]]
+
+# #
