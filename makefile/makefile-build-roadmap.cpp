c69952882113b79cf7998f0796b6fc09992223db
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-12-10 11:56:42 +0800

    Create makefile-build-roadmap.cpp
    Add a reference for building Makefile on mac.

---
+++ makefile/makefile-build-roadmap.cpp
@@ -0,0 +1,18 @@
+//
+//
+//
+//
+/*
+
+
+Other reference:
+1.homebrew-core/Formula/makefile2graph.rb:
+desc "Create a graph of dependencies from GNU-Make"
+  homepage "https://github.com/lindenb/makefile2graph"
+  sha256 "9464c6c1291609c211284a9889faedbab22ef504ce967b903630d57a27643b40"
+  license "MIT"
+  head "https://github.com/lindenb/makefile2graph.git"
+https://github.com/Homebrew/homebrew-core/blob/master/Formula/makefile2graph.rb
+
+*/
+//
