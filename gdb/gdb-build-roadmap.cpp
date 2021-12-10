9c158fbbbdf6604e65174f5fc53533ff0a21a199
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-12-10 12:00:50 +0800

    Create gdb-build-roadmap.cpp
    Add a reference to build GDB.

---
+++ gdb/gdb-build-roadmap.cpp
@@ -0,0 +1,22 @@
+//
+//
+//
+//
+/*
+
+
+
+
+Other reference:
+1.homebrew-core/Formula/gdb.rb:
+desc "GNU debugger"
+  homepage "https://www.gnu.org/software/gdb/"
+  sha256 "cccfcc407b20d343fb320d4a9a2110776dd3165118ffd41f4b1b162340333f94"
+  license "GPL-3.0-or-later"
+  revision 1
+  head "https://sourceware.org/git/binutils-gdb.git", branch: "master"
+https://github.com/Homebrew/homebrew-core/blob/master/Formula/gdb.rb
+
+*/
+//
+//
