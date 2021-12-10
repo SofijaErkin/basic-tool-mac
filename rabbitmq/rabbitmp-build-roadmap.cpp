2b568a063f58605a4bfd1cda21205657bc5c022d
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-12-10 10:55:09 +0800

    Update rabbitmp-build-roadmap.cpp
    Add a set of references for building RabbitMQ on mac.

--- rabbitmq/rabbitmp-build-roadmap.cpp
+++ rabbitmq/rabbitmp-build-roadmap.cpp
@@ -1,4 +1,16 @@
 // This is a roadmap of building rabbit on mac.
 /*
 
+Other reference:
+1.
+homebrew-core/Formula/rabbitmq.rb:
+("Messaging broker")
+https://github.com/Homebrew/homebrew-core/blob/master/Formula/rabbitmq.rb
+
+2.
+homebrew-core/Formula/rabbitmq-c.rb:
+("C AMQP client library for RabbitMQ")
+https://github.com/Homebrew/homebrew-core/blob/master/Formula/rabbitmq-c.rb
+
+
 */
