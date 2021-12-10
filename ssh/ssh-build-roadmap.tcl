1cc8d6b3ac98db841b4137d79a5b5e4dd9d6c931
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-12-10 10:43:44 +0800

    Update ssh-build-roadmap.tcl
    Add a set of references for building SSH on mac.

--- ssh/ssh-build-roadmap.tcl
+++ ssh/ssh-build-roadmap.tcl
@@ -2,5 +2,43 @@
 # '#' just for only one line comment
 if 0 {
 
+
+Other reference:
+1.homebrew-core/Formula/sshtrix.rb:
+("SSH login cracker")
+https://github.com/Homebrew/homebrew-core/blob/master/Formula/sshtrix.rb
+
+2.homebrew-core/Formula/sshguard.rb:
+("Protect from brute force attacks against SSH")
+https://github.com/Homebrew/homebrew-core/blob/master/Formula/sshguard.rb
+
+3.homebrew-core/Formula/ssh-vault.rb:
+("Encrypt/decrypt using SSH keys")
+https://github.com/Homebrew/homebrew-core/blob/master/Formula/ssh-vault.rb
+
+4.homebrew-core/Formula/ssh-copy-id.rb:
+("Add a public key to a remote machine's authorized_keys file")
+https://github.com/Homebrew/homebrew-core/blob/master/Formula/ssh-copy-id.rb
+
+5.homebrew-core/Formula/ssh-permit-a38.rb:
+("Central management and deployment for SSH keys")
+https://github.com/Homebrew/homebrew-core/blob/master/Formula/ssh-permit-a38.rb
+
+6.homebrew-core/Formula/git-ssh.rb:
+("Proxy for serving git repositories over SSH")
+https://github.com/Homebrew/homebrew-core/blob/master/Formula/git-ssh.rb
+
+7.homebrew-core/Formula/assh.rb:
+("Advanced SSH config - Regex, aliases, gateways, includes and dynamic hosts")
+https://github.com/Homebrew/homebrew-core/blob/master/Formula/assh.rb
+
+8.homebrew-core/Formula/openssh.rb:
+("OpenBSD freely-licensed SSH connectivity tools")
+https://github.com/Homebrew/homebrew-core/blob/master/Formula/openssh.rb
+
+9.homebrew-core/Formula/stormssh.rb:
+("Command-line tool to manage your ssh connections")
+https://github.com/Homebrew/homebrew-core/blob/master/Formula/stormssh.rb
+
 }
 # "if 0 { ... } " for more than one lines comment.
