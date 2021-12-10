e164e2eedd48bdda445f03d9a8b3d9a675b8a154
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-12-10 11:48:02 +0800

    Create c11-build-roadmap.cmake
    Add a set of references to build C11 on mac.

---
+++ c11/c11-build-roadmap.cmake
@@ -0,0 +1,17 @@
+# #
+# #
+# #
+# [[
+
+
+Other reference:
+1.
+homebrew-core/Formula/clang-format@11.rb:
+("Formatting tools for C, C++, Obj-C, Java, JavaScript, TypeScript")
+homepage "https://clang.llvm.org/docs/ClangFormat.html"
+ sha256 "ce8508e318a01a63d4e8b3090ab2ded3c598a50258cc49e2625b9120d4c03ea5"
+  license "Apache-2.0"
+https://github.com/Homebrew/homebrew-core/blob/master/Formula/clang-format@11.rb
+ 
+]]
+# #
