1acdf0ae6fc9cf6b3bef8462776200b390d3d2cd
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-12-10 11:53:21 +0800

    Create cmake-build-roadmap.cmake
    Add a set of references to build CMake on mac.

---
+++ cmake/cmake-build-roadmap.cmake
@@ -0,0 +1,42 @@
+# #
+# #
+# #
+# #
+# #
+# [[
+
+Other reference:
+1.homebrew-core/Formula/cmake.rb:
+desc "Cross-platform make"
+  homepage "https://www.cmake.org/"
+  sha256 "0e998229549d7b3f368703d20e248e7ee1f853910d42704aa87918c213ea82c0"
+  license "BSD-3-Clause"
+  head "https://gitlab.kitware.com/cmake/cmake.git", branch: "master"
+https://github.com/Homebrew/homebrew-core/blob/master/Formula/cmake.rb
+
+2.homebrew-core/Formula/cmake-docs.rb:
+desc "Documentation for CMake"
+  homepage "https://www.cmake.org/"
+  sha256 "0e998229549d7b3f368703d20e248e7ee1f853910d42704aa87918c213ea82c0"
+  license "BSD-3-Clause"
+  head "https://gitlab.kitware.com/cmake/cmake.git", branch: "master"
+https://github.com/Homebrew/homebrew-core/blob/master/Formula/cmake-docs.rb
+
+3.homebrew-core/Formula/extra-cmake-modules.rb:
+desc "Extra modules and scripts for CMake"
+  homepage "https://api.kde.org/frameworks/extra-cmake-modules/html/index.html"
+  sha256 "33bd83908daa531654455b77fc121b598f757aadf8ba01dbacfda8b8fb050319"
+  license all_of: ["BSD-2-Clause", "BSD-3-Clause", "MIT"]
+  head "https://invent.kde.org/frameworks/extra-cmake-modules.git", branch: "master"
+https://github.com/Homebrew/homebrew-core/blob/master/Formula/extra-cmake-modules.rb
+
+4.homebrew-core/Formula/colormake.rb :
+desc "Wrapper around make to colorize the output"
+  homepage "https://github.com/pagekite/Colormake"
+  sha256 "a3f9fae9a455ac96be1cce0371b28bda33a9af73b06fa8e4329aa2f693d68d22"
+  license "GPL-2.0"
+  head "https://github.com/pagekite/Colormake.git", branch: "master"
+https://github.com/Homebrew/homebrew-core/blob/master/Formula/colormake.rb
+
+]]
+# #
