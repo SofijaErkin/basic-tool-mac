d4387f4ccd3f332fdbb0ad5d62c99e03d61ad7ec
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-12-10 09:00:23 +0800

    Create mysql-build-roadmap.cpp
    Add a set of references for building MySQL on mac.

---
+++ mysql/mysql-build-roadmap.cpp
@@ -0,0 +1,13 @@
+//
+//
+//
+//
+/*
+
+
+Others reference：
+1.homebrew-core/Formula/mysql.rb:
+https://github.com/Homebrew/homebrew-core/blob/master/Formula/mysql.rb
+2.homebrew-core/Formula/mysql@5.7.rb:
+https://github.com/Homebrew/homebrew-core/blob/master/Formula/mysql@5.7.rb
+*/
