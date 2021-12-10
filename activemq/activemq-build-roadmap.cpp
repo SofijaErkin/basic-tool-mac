abd1ac8baba2f96dcd4a669500a51444e3192b33
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-12-10 10:51:45 +0800

    Update activemq-build-roadmap.cpp
    Add a set of references for building ActiveMQ on mac.

--- activemq/activemq-build-roadmap.cpp
+++ activemq/activemq-build-roadmap.cpp
@@ -1,4 +1,16 @@
 // ActiveMQ building roadmap
 /*
 
+Other reference:
+1.homebrew-core/Formula/activemq.rb:
+("Apache ActiveMQ: powerful open source messaging server")
+https://github.com/Homebrew/homebrew-core/blob/master/Formula/activemq.rb
+
+
+2.homebrew-core/Formula/activemq-cpp.rb :
+("C++ API for message brokers such as Apache ActiveMQ")
+https://github.com/Homebrew/homebrew-core/blob/master/Formula/activemq-cpp.rb
+
+
+
 */
