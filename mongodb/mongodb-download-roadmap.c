291fa9e8af569c9feb4e2a90295705d53dd46948
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-12-14 07:38:56 +0800

    Update mongodb-download-roadmap.c
    Deleted some drafts of the roadmap.

--- mongodb/mongodb-download-roadmap.c
+++ mongodb/mongodb-download-roadmap.c
@@ -330,69 +330,6 @@ link of the visualization tool is:
 
 https://studio3t.com/download/?utm_source=intellishell&utm_medium=welcomepage&utm_campaign=inapp-robo3t
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-2.sudo ln -s;
-
-3.$Path;
-
-4.vim /usr/local/mongod.conf;
-
-5.sudo mkdir -p /data/db;
-
-6.sudo mkdir -p /usr/local/var/log/mongodb;
-
-7.sudo chown -R /data/db;
-
-8.sudo chown -R /usr/local/var/log/mongodb;
-
-9. ./mongod ./etc/mongodb/conf;
-
-10.mongod;
-
-11.mongo;
-
-12.use admin;
-
-13.db.createUser({user: "root",pwd: "password", roles: [ { role: "root", db: "admin" } ]})
-
-14.db.shutdownServer();
-
-15. user-root user-pass
-
-16.VS code or others Desktop, eg:mongo studio3T.
-
 The MongoDB version suitable for macOS 1x.x ranges from 4.0.x to 6.x.x, or even the lastest version.
 
 Others reference：
@@ -409,4 +346,5 @@ https://github.com/Homebrew/homebrew-core/blob/master/Formula/mongo-cxx-driver.r
 
 4.homebrew-core/Formula/mongo-c-driver.rb:("C driver for MongoDB")
 https://github.com/Homebrew/homebrew-core/blob/master/Formula/mongo-c-driver.rb
+
 */
