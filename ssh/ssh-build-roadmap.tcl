ad6e6767b0e2fd8639f1a951916b958ee68183fc
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-12-15 14:33:31 +0800

    Update ssh-build-roadmap.tcl
    Add a set of Secure Shell statements.

--- ssh/ssh-build-roadmap.tcl
+++ ssh/ssh-build-roadmap.tcl
@@ -2,6 +2,45 @@
 # '#' just for only one line comment
 if 0 {
 
+Secure Shell (SSH) is a cryptographic network protocol for operating network 
+
+services securely over an unsecured network.
+
+Typical applications include remote command-line, login, and remote command 
+
+execution, but any network service can be secured with SSH. 
+
+SSH provides a secure channel over an unsecured network by using a client
+
+–server architecture, connecting an SSH client application with an SSH server.
+
+The protocol specification distinguishes between two major versions, referred 
+ 
+to as SSH-1 and SSH-2. 
+
+The standard TCP port for SSH is 22. 
+
+SSH is generally used to access Unix-like operating systems, but it can also 
+
+be used on Microsoft Windows.
+
+Windows 10 uses OpenSSH as its default SSH client and SSH server.
+
+SSH was designed as a replacement for Telnet and for unsecured remote shell 
+
+protocols such as the Berkeley rsh and the related rlogin and rexec protocols. 
+
+Those protocols send sensitive information, notably passwords, in plaintext, 
+
+rendering them susceptible to interception and disclosure using packet 
+
+analysis.
+
+The encryption used by SSH is intended to provide confidentiality and 
+
+integrity of data over an unsecured network, such as the Internet. 
+
+
 
 Other reference:
 1.homebrew-core/Formula/sshtrix.rb:
