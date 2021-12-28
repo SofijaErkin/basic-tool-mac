ba1049c822681e197e38cda3ebbe4ce4920d8935
Vittore Marcas <matthiasxywilliams@gmail.com>
2021-12-28 15:50:22 +0800

    Update git-build-roadmap.cmake
    Add a way to end the conflict between two branches.

--- git/git-build-roadmap.cmake
+++ git/git-build-roadmap.cmake
@@ -105,6 +105,85 @@ this kind of mark, comment out the feature ("`ae9a0f6b7e42fda2ce9b14a21a7a03cfc5
 
 `git push`
 
+### Merge Two branches
+
+(1) Enter the directory of your needed Merging Repository
+
+eg. cd /Users/marryme/GitHub/Personal/Sofija/hello-world-debug-vscode
+
+(2)To view all remote branches
+
+`git branch -a`
+
+discover Terminal:
+
+`* main`
+  `remotes/origin/HEAD -> origin/main`
+ ` remotes/origin/main`
+ ` remotes/origin/matMain`
+ ` remotes/origin/taylorPatch`
+  `remotes/origin/DavidPatch`
+
+(3)Switch branch
+
+For example, there are three people developing at the same time, `matMain` was 
+
+originally based on `taylorPatch`, but because the project has not been released, 
+
+`taylorPatch`, `DavidPatch`, and `matMain` are all developed at the same time. Now I 
+
+want to merge the added functions of `taylorPatch` to `matMain`;
+
+Steps at this time: check `taylorPatch` and  `matMain`:
+
+`git checkout taylorPatch`
+
+`git checkout matMain`
+
+Then execute merge based on the branch of `matMain`:
+
+`git merge taylorPatch`
+
+If there is no error, then directly submit the code:
+
+`git push origin matMain`
+
+If an error is reported, it is basically a conflict(reference:
+
+`Fixed conflicts in uploading GitHub files`):
+
+eg:
+
+`CONFLICT (content): Merge conflict in src/main/bank_simulate.cc`
+
+`Auto-merging inc/hand_make_line.h.`
+
+`CONFLICT (content): Merge conflict in inc/hand_make_line.h`
+
+`Automatic merge failed; fix conflicts and then commit the result.`
+
+You need to go to the prompt file and comment out the version conflict 
+
+automatically marked by git, and delete it according to the functions 
+
+you need.
+
+Then put the conflicting files `git add`, and `commit`.
+
+For example, if you have two conflicting files, separate them with spaces 
+
+when adding multiple files.
+
+`git add src/main/bank_simulate.cc inc/hand_make_line.h`
+
+Finally commit:
+
+`git commit -m "end the conflict between two branch"`
+
+Submit local repository:
+
+`git push origin matMain`
+
 ## 3.Remote commands
 
 ### View all remote branches
