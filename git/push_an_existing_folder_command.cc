27f8999ace40782b89b454e45fd4c8001cb4335d
SofijaMarcas <matthiasxywilliams@gmail.com>
2022-01-02 08:57:20 +0800

    Create push_an_existing_folder_command.cc

---
+++ git/push_an_existing_folder_command.cc
@@ -0,0 +1,19 @@
+cd existing_folder
+
+git init
+
+git config --list
+
+git config user.name "Sofija_Personal_GitHub"
+
+git config user.email "sofijapersonal@gmail.com"
+
+git add/rm <file>
+
+git commit -m "XXXX"
+
+git branch -M main
+
+git remote add origin git@sofija_persaonal-github.com:SofijaPersonal/hello-world-debug-vscode.git
+
+git push -u origin main
