0b092a7c12737ea6c5de6ba5d94fcb4af1c492f1
SofijaMarcas <matthiasxywilliams@gmail.com>
2022-02-14 15:33:20 +0800

    Update cmakeversion

--- vscode/build_compile_debug_case_combat/onlyOneSimpleSourceFile/cmakeversion/CTestConfig.cmake
+++ vscode/build_compile_debug_case_combat/onlyOneSimpleSourceFile/cmakeversion/CTestConfig.cmake
@@ -1,6 +1,9 @@
 set(CTEST_PROJECT_NAME "CMaketheFitBody")
+# The project name
 set(CTEST_NIGHTLY_START_TIME "00:00:00 UTC")
+# The project "Nightly" start time
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "my.cdash.org")
 set(CTEST_DROP_LOCATION "/submit.php?project=CMaketheFitBody")
+# The URL of the CDash instance where the submission's generated documents will be sent
 set(CTEST_DROP_SITE_CDASH TRUE)
