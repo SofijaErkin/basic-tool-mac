137d41708e58c0f5dcf223c2f5965f61a3c49432
SofijaMarcas <matthiasxywilliams@gmail.com>
2022-03-14 14:19:51 +0800

    Update cmakeversion

---
+++ vscode/build_compile_debug_case_combat/onlyOneSimpleSourceFile/cmakeversion/vsCodeCMake/practiseCMake/include/Pet.h
@@ -0,0 +1,8 @@
+#include <string>
+class Pet{
+    private:std::string name;
+    public:Pet(std::string& name);
+    void sound();
+    void eat();
+    std::string& getName();
+};
--- vscode/build_compile_debug_case_combat/onlyOneSimpleSourceFile/cmakeversion/vsCodeCMake/practiseCMake/main.cpp
+++ vscode/build_compile_debug_case_combat/onlyOneSimpleSourceFile/cmakeversion/vsCodeCMake/practiseCMake/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
-
+#include "include/Pet.h"
+#include <memory>
 int main(int, char**) {
     std::cout << "Hello, world!\n";
+    std::string n = "cat";
+    std::unique_ptr<Pet> pet(new Pet(n));
+    pet->eat();
 }
