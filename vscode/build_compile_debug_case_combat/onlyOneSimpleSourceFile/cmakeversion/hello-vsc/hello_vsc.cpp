8649e1242a598ef33f820b16e411f1a5464b3afd
SofijaMarcas <matthiasxywilliams@gmail.com>
2022-03-17 09:22:25 +0800

    Update cmakeversion

--- vscode/build_compile_debug_case_combat/onlyOneSimpleSourceFile/cmakeversion/hello-vsc/hello_vsc.cpp
+++ vscode/build_compile_debug_case_combat/onlyOneSimpleSourceFile/cmakeversion/hello-vsc/hello_vsc.cpp
@@ -1,3 +1,4 @@
+// Reference: https://medium.com/@ivan.mejia/c-development-using-visual-studio-code-cmake-and-lldb-d0f13d38c563
 #include <iostream>
 int main(int argc, const char * argv[]) {
     std::cout << "hello Visual Studio Code! :)" << '\n'; 
