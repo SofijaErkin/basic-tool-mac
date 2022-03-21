8331a6c6fc1abffe5889afd2eb004686078418f0
SofijaMarcas <matthiasxywilliams@gmail.com>
2022-03-21 11:37:16 +0800

    Delete practiseCMake hello-vsc folders

--- vscode/build_compile_debug_case_combat/onlyOneSimpleSourceFile/cmakeversion/hello-vsc/hello_vsc.cpp
+++
@@ -1,6 +0,0 @@
-// Reference: https://medium.com/@ivan.mejia/c-development-using-visual-studio-code-cmake-and-lldb-d0f13d38c563
-#include <iostream>
-int main(int argc, const char * argv[]) {
-    std::cout << "hello Visual Studio Code! :)" << '\n'; 
-    return 0;
-}
