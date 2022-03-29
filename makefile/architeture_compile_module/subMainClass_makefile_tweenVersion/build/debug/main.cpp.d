508f3aa078782645999be205122466ac69261eff
SofijaMarcas <matthiasxywilliams@gmail.com>
2022-03-29 19:48:07 +0800

    Create subMainClass_makefile_tweenVersion

---
+++ makefile/architeture_compile_module/subMainClass_makefile_tweenVersion/build/debug/main.cpp.d
@@ -0,0 +1,3 @@
+build/debug/main.cpp.o: apps/main.cpp apps/../common/include/Pet.h
+
+apps/../common/include/Pet.h:
---
+++ makefile/architeture_compile_module/subMainClass_makefile_tweenVersion/common/include/Pet.h
@@ -0,0 +1,12 @@
+// Reference: https://medium.com/@dexterchan_44737/visual-studio-code-build-and-debug-a-c-with-cmake-on-mac-os-7633bc59bf34
+#include <iostream> 
+#include <string>
+class Pet{
+    private:std::string name;
+    public:Pet(std::string& name);
+    void sound();
+    void eat();
+    std::string& getName();
+};
+
+
