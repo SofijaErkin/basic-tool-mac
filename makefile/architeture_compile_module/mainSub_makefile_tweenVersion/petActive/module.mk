cf4e2bc25c39daede30fd6932b7a0b344644feb6
SofijaMarcas <matthiasxywilliams@gmail.com>
2022-03-29 10:35:10 +0800

    Update mainSub_makefile_tweenVersion

--- makefile/architeture_compile_module/mainSub_makefile_tweenVersion/petActive/module.mk
+++ makefile/architeture_compile_module/mainSub_makefile_tweenVersion/petActive/module.mk
@@ -1,5 +1,7 @@
 SRC_DIRS += ./petActive/source
-echo $(SRC_DIRS)
+#.PHONY: all_a
+all_a:
+	echo $(SRC_DIRS)
 # SRCS := $(shell find $(SRC_DIRS) -name '*.cpp' -or -name '*.c' -or -name '*.s')
 #echo $(SRC_DIRS)
 #OBJS := $(patsubst %.cpp, ${BUILD_DIR}/%.cpp.o, $(notdir $(SRCS)))
@@ -7,6 +9,8 @@ echo $(SRC_DIRS)
 #DEPS := $(OBJS:.o=.d)
 #echo $(SRC_DIRS)
 INC_DIRS += ./petActive/include
-echo $(SRC_DIRS)
+#.PHONY: all_b
+all_b:
+	echo $(INC_DIRS)
 #INC_FLAGS := $(addprefix -I,$(INC_DIRS))
 #echo $(SRC_DIRS)
